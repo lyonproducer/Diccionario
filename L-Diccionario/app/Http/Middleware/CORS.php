89c868aac5ccc93b538de71641f55ab0b41d4473
lyonproducer <hernandezleonardo085@gmail.com>
2018-12-17 13:02:17 -0400

    cors prueba

--- L-Diccionario/app/Http/Middleware/CORS.php
+++ L-Diccionario/app/Http/Middleware/CORS.php
@@ -18,9 +18,9 @@ class CORS
         $response = $next($request);
 
         $response->headers->set('Access-Control-Allow-Origin' , '*');
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Application');
         
-        return $response;  
+        return $response;    
     }
 }
